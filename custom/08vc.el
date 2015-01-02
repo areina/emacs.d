0af710f5b504006015c18e1be0107fd5c068bbfb
Toni Reina <areina0@gmail.com>
2015-01-02 09:45:31 +0100

    Add function for show merged info in magit branch manager

--- custom/08vc.el
+++ custom/08vc.el
@@ -10,6 +10,26 @@
 (use-package magit
   :bind ("C-x g" . magit-status))
 
+(defun t-magit-branches-show-merged (origin-fun)
+  "Call magit-refresh-branch-manager and add info about merged branches.
+On magit branch manager, add [merged] in the end of each branch line if this one
+ is already merged."
+  (funcall origin-fun)
+  (goto-char (point-min))
+  (let ((merged-branches (mapcar (lambda (b) (s-trim b))
+  				 (magit-git-lines "branch" "--merged" "master"))))
+    (while (progn
+  	     (let ((branch-at-point (progn
+  				      (search-forward "   ")
+  				      (thing-at-point 'symbol))))
+  	       (when (member branch-at-point merged-branches)
+  		 (end-of-line)
+  		 (insert "[merged]")))
+  	     (forward-line 1)
+  	     (not (looking-at "^$"))))))
+
+(advice-add 'magit-refresh-branch-manager :around #'t-magit-branches-show-merged)
+
 (eval-after-load 'magit
   '(define-key magit-mode-map "#gg"
      'endless/load-gh-pulls-mode))

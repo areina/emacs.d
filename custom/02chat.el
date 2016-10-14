de9cd5e40c26bcd8d47f4fe573cb451f483751fc
Toni Reina <areina0@gmail.com>
2016-10-14 15:28:23 +0200

    Add functions to mark erc buffers as read

--- custom/02chat.el
+++ custom/02chat.el
@@ -101,6 +101,58 @@
 	(add-hook 'erc-send-modify-hook 'erc-replace-insert)))
     (erc-update-modules)))
 
+(defun endless/mark-read ()
+  "Mark buffer as read up to current line."
+  (message "foo")
+  (let ((inhibit-read-only t))
+    (put-text-property
+     (point-min) (- (line-beginning-position) 5)
+     'face       'font-lock-comment-face)))
+
+(defun endless/bury-buffer ()
+  "Bury buffer and maybe close its window."
+  (interactive)
+  (message "foo")
+  (endless/mark-read)
+  (bury-buffer)
+  (when (cdr (window-list nil 'nomini))
+    (delete-window)))
+
+(eval-after-load 'erc
+  '(define-key erc-mode-map (kbd "<escape>")
+     #'endless/bury-buffer))
+
+(defun erc-track-switch-buffer (arg)
+  "Switch to the next active ERC buffer, or if there are no active buffers,
+switch back to the last non-ERC buffer visited.  Next is defined by
+`erc-track-switch-direction', a negative argument will reverse this."
+  (interactive "p")
+  (if (not erc-track-mode)
+      (message (concat "Enable the ERC track module if you want to use the"
+		       " tracking minor mode"))
+    (cond (erc-modified-channels-alist
+	   ;; if we're not in erc-mode, set this buffer to return to
+	   (unless (eq major-mode 'erc-mode)
+	     (validate-setq erc-track-last-non-erc-buffer (current-buffer)))
+	   ;; hack
+	   (when (eq major-mode 'erc-mode)
+	     (endless/bury-buffer))
+	   ;; and jump to the next active channel
+	   (switch-to-buffer (erc-track-get-active-buffer arg)))
+	  ;; if no active channels, switch back to what we were doing before
+	  ((and erc-track-last-non-erc-buffer
+		erc-track-switch-from-erc
+		(buffer-live-p erc-track-last-non-erc-buffer))
+	   ;; hack
+	   (when (eq major-mode 'erc-mode)
+	     (endless/bury-buffer))
+	   (switch-to-buffer erc-track-last-non-erc-buffer)))))
+
+(defun erc-track-reset ()
+  (interactive)
+  (validate-setq erc-modified-channels-alist nil)
+  (erc-modified-channels-update))
+
 ;;
 ;; JABBER
 ;;

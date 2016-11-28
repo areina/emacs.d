4c13e37cce38aa25a7a3e34c7c2a4a717287c927
Toni Reina <areina0@gmail.com>
2016-11-28 01:59:22 +0100

    Remove unused code from org config

--- custom/06org.el
+++ custom/06org.el
@@ -44,36 +44,5 @@
 		("w" "org-protocol" entry (file "~/.emacs.d/org/refile.org")
 		 "* TODO Review %c\n%U\n" :immediate-finish t)))))
 
-(use-package org-protocol
-  :config
-  (progn
-    (add-to-list 'org-protocol-protocol-alist
-		 '("Subscribe to feed with elfeed."
-		   :protocol "elfeed"
-		   :function org-protocol-elfeed))
-    (add-to-list 'org-protocol-protocol-alist
-		 '("Open url with eww."
-		   :protocol "eww"
-		   :function org-protocol-eww))))
-
-;; (load-file (expand-file-name "vendor/org-protocol-github-lines.el" user-emacs-directory))
-;; (validate-setq org-protocol-github-project-directories '("/tmp/"))
-
-;;;###autoload
-(defun org-protocol-elfeed (data)
-  (let ((url (org-protocol-sanitize-uri data)))
-    (elfeed-add-feed url)
-    (customize-save-variable 'elfeed-feeds elfeed-feeds)))
-
-;;;###autoload
-(defun org-protocol-eww (data)
-  (let ((url (org-protocol-sanitize-uri data)))
-    (eww-browse-url url)
-    (raise-frame)))
-
-;; Standard key bindings
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-
 (provide '06org)
 ;;; 06org.el ends here

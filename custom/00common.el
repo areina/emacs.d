77df7a89a1a32bd3b51eba09149add6026bd1c05
Toni Reina <areina0@gmail.com>
2017-03-03 19:43:52 +0100

    Diminish `subword-mode`

--- custom/00common.el
+++ custom/00common.el
@@ -492,6 +492,9 @@
 (use-package prodigy
   :ensure t)
 
+(use-package subword
+  :diminish subword-mode)
+
 (defun endless/comment-line (n)
   "Comment or uncomment current line and leave point after it.
 With positive prefix, apply to N lines including current one.
